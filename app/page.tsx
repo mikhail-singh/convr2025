d6e35317bcdafd8459d63a4f5d5a75a28702b169
Mikhail <mikhail@kandk.studio>
2024-12-19 09:48:17 +1100

    Temporarily removed marquee

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import Banner2 from "./components/Banner2";
 import Contact from "./components/Contact";
 import ImgTextBanner from "./components/ImgTextBanner";
 import Landing from "./components/Landing";
-import Marquee from "./components/Marquee";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Landing />
-      <Marquee />
       <Banner
         text1="25th international conference on construction applications of Virtual Reality"
         text2="Bridging Global Disparities: Harnessing Digital Technology for Equitable and Net-Zero Development"
